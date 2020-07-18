9faca3d72606e339e3224babd79ad0e0f23d309d
Loquen Jones <lrjones416@gmail.com>
2020-07-18 10:16:39 -0700

    add book table and foreign key connection

--- app/models.py
+++ app/models.py
@@ -5,6 +5,16 @@ class User(db.Model):
   username = db.Column(db.String(64), index=True, unique=True)
   email = db.Column(db.String(120), index=True, unique=True)
   password_hash = db.Column(db.String(128))
+  books = db.relationship('Book', backref='owner', lazy='dynamic')
 
   def __repr__(self):
     return '<User {}>'.format(self.username)
+
+class Book(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  author = db.Column(db.String(120), index=True)
+  title = db.Column(db.String(120), index=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+  def __repr__(self):
+    return '<Book {}>'.format(self.body)
