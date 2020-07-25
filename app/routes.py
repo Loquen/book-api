5ad19499ecb047b09f91f2570af9e7a28b1122a5
Loquen Jones <lrjones416@gmail.com>
2020-07-25 11:53:54 -0700

    add login and form functionality

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
+from flask import render_template
 from app import app, db
+from flask_login import current_user, login_user, logout_user
 from app.models import User, Book
+from app.forms import LoginForm
 
 @app.shell_context_processor
 def make_shell_context():
@@ -8,4 +11,25 @@ def make_shell_context():
 @app.route('/')
 @app.route('/index')
 def index():
-  return {'test': 'hello json world'}
+  user = {'username': 'Loquen'}
+  # return {'test': 'hello json world'} #JSON
+  return render_template('index.html', title='Home', user=user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
+  form = LoginForm()
+  if form.validate_on_submit():
+    user = User.query.filter_by(username=form.username.data).first()
+    if user is None or not user.check_password(form.password.data):
+      flash('Invalid username or password')
+      return redirect(url_for('login'))
+    login_user(user, remember=form.remember_me.data)
+    return redirect(url_for('index'))
+  return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+  logout_user()
+  return redirect(url_for('index'))
