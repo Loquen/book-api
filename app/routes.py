edb87c83d79b340a98a33dd5e9c714e8c2771504
Loquen Jones <lrjones416@gmail.com>
2020-07-26 13:47:14 -0700

    fake book data and current user auth

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.urls import url_parse
 from app import app, db
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Book
 from app.forms import LoginForm
 
@@ -10,10 +11,20 @@ def make_shell_context():
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-  user = {'username': 'Loquen'}
   # return {'test': 'hello json world'} #JSON
-  return render_template('index.html', title='Home', user=user)
+  books = [
+    {
+      'title': "A Space Odessey",
+      'author': "Arthur C. Clarke",
+    },
+    {
+      'title': "Ender's game",
+      'author': "Orson Scott Card",
+    }
+  ]
+  return render_template('index.html', title='Home', books=books)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -26,7 +37,10 @@ def login():
       flash('Invalid username or password')
       return redirect(url_for('login'))
     login_user(user, remember=form.remember_me.data)
-    return redirect(url_for('index'))
+    next_page = request.args.get('next')
+    if not next_page or url_parse(next_page).netloc != '':
+      next_page = url_for('index')
+    return redirect(next_page)
   return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
