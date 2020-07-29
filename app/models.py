b39e15d65a82f3f957130b49448860abf5531815
Loquen Jones <lrjones416@gmail.com>
2020-07-28 17:50:31 -0700

    add user signup, avatars and use of templates

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db
 from app import login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 class User(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -19,9 +20,15 @@ class User(UserMixin, db.Model):
   def check_password(self, password):
     return check_password_hash(self.password_hash, password)
 
+  def avatar(self, size):
+    digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+    return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
 @login.user_loader
 def load_user(id):
   return User.query.get(int(id))
+
+  # Users: lo = pass1234, three = pass1234
 
 class Book(db.Model):
   id = db.Column(db.Integer, primary_key=True)
--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from werkzeug.urls import url_parse
 from app import app, db
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Book
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 
 @app.shell_context_processor
 def make_shell_context():
@@ -47,3 +47,27 @@ def login():
 def logout():
   logout_user()
   return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    user = User(username=form.username.data, email=form.email.data)
+    user.set_password(form.password.data)
+    db.session.add(user)
+    db.session.commit()
+    flash('Congratulations, you are now a registered user!')
+    return redirect(url_for('login'))
+  return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+  user = User.query.filter_by(username=username).first_or_404()
+  books = [
+    {'author': 'test1', 'title': 'Test Book 1'},
+    {'author': 'test2', 'title': 'Test Book 2'},
+  ]
+  return render_template('user.html', user=user, books=books)
